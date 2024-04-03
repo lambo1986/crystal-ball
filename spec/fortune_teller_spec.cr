0eaf3adefe7040ffe7904331bd494cc24ad46ac3
Nathan Lambertson <lambertones@gmail.com>
2024-04-02 22:58:27 -0500

    Create tests and fixture for fortunes and update the FortuneTeller class

--- spec/fortune_teller_spec.cr
+++ spec/fortune_teller_spec.cr
@@ -2,10 +2,22 @@ require "./spec_helper"
 require "../src/fortune_teller"
 
 describe FortuneTeller do
-  it "tells a fortune" do
-    fortune_teller = FortuneTeller.new
+  it "loads fortunes from a file" do
+    fortunes_path = "spec/fixtures/test_fortunes.txt"
+    fortune_teller = FortuneTeller.new(fortunes_path)
+    fortunes = fortune_teller.fortunes 
+
+    fortunes.should_not be_nil
+    fortunes.size.should eq 3
+    fortunes.first.should eq "Fortune 1"
+  end
+
+  it "returns a random fortune" do
+    fortunes_path = "spec/fixtures/test_fortunes.txt"
+    fortune_teller = FortuneTeller.new(fortunes_path)
     fortune = fortune_teller.tell_fortune
-    fortunes = ["Good things are coming your way!", "Be wary of unexpected emails.", "An exciting opportunity lies ahead."]
+    fortunes = fortune_teller.fortunes 
+
     fortunes.should contain(fortune)
   end
 end
--- src/fortune_teller.cr
+++ src/fortune_teller.cr
@@ -1,6 +1,15 @@
 class FortuneTeller
+  @@fortunes : Array(String)?# must define the type pf data and use ? to check if it's nil
+
+  def initialize(path : String)
+    @@fortunes = File.read_lines(path)
+  end
+
   def tell_fortune
-    fortunes = ["Good things are coming your way!", "Be wary of unexpected emails.", "An exciting opportunity lies ahead."]
-    fortunes.sample
+    @@fortunes.not_nil!.sample# select random fortune and check for nil
+  end
+
+  def fortunes# makes the array of fortunes available outside of the class
+    @@fortunes.not_nil!
   end
 end
