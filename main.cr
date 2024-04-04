b14b254feeff6377deb41245b7f66ba8e4d19ef4
Nathan Lambertson <lambertones@gmail.com>
2024-04-03 21:14:05 -0500

    Add very basic ANIMATION!!

--- main.cr
+++ main.cr
@@ -1,8 +1,49 @@
 require "colorize"
 require "./src/fortune_teller"
 
+def animate_speech(duration, mouth_open, mouth_closed)
+  frames = [mouth_open, mouth_closed]
+  start_time = Time.local
+  
+  while Time.local < start_time + duration.seconds
+    system("clear") || system("cls")
+    frame = frames[Time.local.second % 2]
+    puts frame
+    sleep 0.5 # timing for mouth movement
+  end
+end
 
 def main
+  mouth_open = "
+               __||__
+              /      \\
+             /        \\
+            /   o   o  \\
+           (     >Y<    )
+            \\    <O>   /
+             | '.___.' |
+         ____|  |___|  |___
+        /     \\______/     \\
+       /  / \\          / \\  \\
+      /  /   \\        /   \\  \\
+".colorize(:red)
+
+mouth_closed = "
+               __||__
+              /      \\
+             /        \\
+            /   o   o  \\
+           (     >Y<    )
+            \\    -^-   /
+             | '.___.' |
+         ____|  |___|  |___
+        /     \\______/     \\
+       /  / \\          / \\  \\
+      /  /   \\        /   \\  \\
+".colorize(:red)
+
+
+
   system("clear") || system("cls")
   puts "
       ___
@@ -19,7 +60,6 @@ def main
 
   loop do
     fortune = FortuneTeller.new("./resources/fortunes.txt").tell_fortune
-
     response_templates = [
       "Dear #{name}, the stars whisper to me that: #{fortune}",
       "Ah, #{name}, fate has a message for you: #{fortune}",
@@ -39,21 +79,21 @@ def main
     ]
 
     selected_template = response_templates.sample
+    speech_duration = 8 # adjust for animation duration
+
+    spawn do # kinda like threads in ruby?
+      animate_speech(speech_duration, mouth_open, mouth_closed)
+    end
+
     puts selected_template.colorize(:light_blue)
-    puts "
-               __||__
-              /      \\
-             /        \\
-            /   o   o  \\
-           (     >Y<    )
-            \\    -^-   /
-             | '.___.' |
-         ____|  |___|  |___
-        /     \\______/     \\
-       /  / \\          / \\  \\
-      /  /   \\        /   \\  \\
-".colorize(:red)
+    sleep 0.5
     Process.run("say", args: [selected_template])
+    puts selected_template.colorize(:light_blue)
+    sleep 1
+
+    system("clear") || system("cls")
+    puts mouth_closed
+    puts selected_template.colorize(:light_blue)
     puts "Would you like another fortune? (Y/N)".colorize(:yellow)
     response = gets.to_s.strip.upcase
     
@@ -62,8 +102,13 @@ def main
       system("clear") || system("cls")
       next 
     when "N"
-      puts "Thank you for visiting the Crystal Ball Fortune Teller. Farewell, #{name}!".colorize(:green)
+      spawn do
+        animate_speech(speech_duration, mouth_open, mouth_closed)
+      end
+      
+      sleep 0.3
       Process.run("say", args: ["Good fortune for you and goodbye!"])
+      puts "Thank you for visiting the Crystal Ball Fortune Teller. Farewell, #{name}!".colorize(:green)
       break 
     else
       puts "
@@ -85,7 +130,7 @@ def main
        |    | |    |
       /     | |     \\
      /      | |      \\
-".colorize(:red)
+      ".colorize(:red)
       puts "Invalid response. Exiting...".colorize(:red)
       Process.run("say", args: ["Good-Bye, Nice Try!"])
       break 
